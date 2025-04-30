cb7b49eb73da09d4401f6129cb9fee87897dfd55
BartoszWezykPortfolio <bwezykpraca@gmail.com>
2025-04-30 20:33:37 +0200

    Time added column created in excel

--- services/get_weather_data.py
+++ services/get_weather_data.py
@@ -5,8 +5,11 @@ from pprint import pprint
 from utils.temp_converter import kelvin_to_celsius
 from utils.wind_converter import ms_to_kmh
 from utils.date_converter import date_format
+import time
 
 def get_weather_data(token: str, city: str):
+
+    timestamp = time.strftime('%H:%M')
 
     url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={token}'
 
@@ -20,7 +23,8 @@ def get_weather_data(token: str, city: str):
             'Temp_Feels': kelvin_to_celsius(data['main']['feels_like']),
             'Humidity': data['main']['humidity'],
             'Wind_Speed': ms_to_kmh(data['wind']['speed']),
-            'Fate': date_format(data['dt'])
+            'Date': date_format(data['dt']),
+            'Time_Added': timestamp
         }
 
         return weather
